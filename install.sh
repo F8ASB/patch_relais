e01ea37eacc3f80ff6296e3065e8e164fa139598
F8ASB <f8asb@hotmail.fr>
2020-12-26 21:50:20 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,40 @@
+
+#!/bin/sh
+
+#copie des fichiers sons
+
+cd /usr/share/svxlink/sounds
+
+git clone https://github.com/F8ASB/fr_FR_Agnes.git
+
+mv /usr/share/svxlink/sounds/fr_FR /usr/share/svxlink/sounds/fr_FR_Old 
+mv /usr/share/svxlink/sounds/fr_FR_Agnes /usr/share/svxlink/sounds/fr_FR 
+
+#recuperation de l'indicatif 
+indicatif=$(grep CALLSIGN svxlink.cfg | head -1| sed 's/.\{9\}//')
+echo "Indicatif du Hotspot: "$indicatif
+
+#changement du nom du repertoire sons perso
+mv /usr/share/svxlink/sounds/fr_FR/INDICATIF_RELAIS /usr/share/svxlink/sounds/fr_FR/$indicatif
+
+#copie des fichiers Logic et RepeaterLogic dans le dossier local
+wget -N -P /usr/share/svxlink/events.d/local https://raw.githubusercontent.com/F8ASB/patch_relais/main/RepeaterLogic.tcl
+wget -N -P /usr/share/svxlink/events.d/local https://raw.githubusercontent.com/F8ASB/patch_relais/main/Logic.tcl
+
+#personnalisation du svxlink.cfg pour le mode relais
+
+sed -i -r 's/.* LOGICS=SimplexLogic,ReflectorLogic.*/ LOGICS=RepeaterLogic,ReflectorLogic/g' /etc/spotnik/svxlink.cfg
+
+sed -i -r 's/.* CALLSIGN=.*/ CALLSIGN=$indicatif/g' /etc/spotnik/data_reapeater.cfg
+
+#ajout des parametres RepeaterLogic dans svxlink.cfg
+sed -i '/LINKS=ALLlink/ { n ; 
+r  data_reapeater.cfg
+}' svxlink.cfg
+
+#ajout indicatif
+sed -i "s/^CALLSIGN=.*/CALLSIGN=$indicatif/" /etc/spotnik/data_reapeater.cfg
+
+
+
+
